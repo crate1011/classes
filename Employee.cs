411de2b0ef48fc71c45b78ea2ae83a8ebb4d9c32
Christian Suriano <cratea113@gmail.com>
2022-08-24 09:24:29 -0700

    first commit

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Classes
+{
+
+    public class Employee
+    {
+        public String FisrtName { get; set; }
+        public String LastName { get; set; }
+        public String Title { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+
+            // Create three employees
+            Employee Merry = new Employee()
+            {
+                FisrtName = "Merry",
+                LastName = "Brandybuck",
+                Title = "Tree Herder",
+                StartDate = new DateTime(1995, 05, 20)
+            };
+            Employee Pippin = new Employee()
+            {
+                FisrtName = "Pippin",
+                LastName = "Took",
+                Title = "Cook",
+                StartDate = new DateTime(1995, 06, 20)
+            };
+            Employee Shagrat = new Employee()
+            {
+                FisrtName = "Shagrat",
+                LastName = "The Orc",
+                Title = "Murderer",
+                StartDate = new DateTime(1996, 07, 30)
+            };
+            // Assign the employees to the company
+            List<Employee> TreeBeardEmployees = new List<Employee>()
+            {
+                Merry, Pippin, Shagrat
+            };
+
+            Company myCompany = new Company("TreeBeards Lawn Care", new DateTime(1995, 5, 15), TreeBeardEmployees);
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+        }
+    }
+}
